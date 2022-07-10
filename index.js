527f1b3121a345adc92a570bc0da90f28dbca885
Madison Mulligan <madisonmulligan97@gmail.com>
2022-07-10 10:22:38 -0400

    Added to the index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const data = () => {
         name: 'choices',
         message: 'What would you like to do?',
         choices: [
-            {name: 'View all Departments', value: 1},
+            { name: 'View all Departments', value: 1},
             { name: 'View all Roles', value: 2 },
             { name: 'View all employees', value: 3 },
             { name: 'Add a department', value: 4 }, 
@@ -21,6 +21,34 @@ const data = () => {
             { name: 'Quit' , value: 8},   
         ]
         }
+        .then((data) => {
+            if ( data.choices === 1 ) {
+                viewAllDepartments();
+            } else if (data.choices === 2 ) {
+                viewAllRoles();
+            } else if (data.choices === 3) {
+                viewAllEmployees();
+            } else if (data.choices === 4) {
+                addDepartment();
+            } else if (data.choices === 5) {
+                addRole();
+            } else if (data.choices === 6) {
+                addEmployee();
+            } else if (data.choices === 7) {
+                updateEmployeeRole();
+            } else if (data.choices === 8) {
+                db.end();
+            }
+        })
+    };
+
+//Choose "View all Departments"
+const viewAllDepartments = () => {
+    const sql = 'SELECT * From department';
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        options();
+    });
     
-    ])
 }
